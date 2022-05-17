212c3dc773991f61b7a1af893f5da644757ae253
Francesco <iudica.francesco@gmail.com>
2022-05-17 11:58:10 +0200

    FINISHED esercizio1.py

--- esercizio1.py
+++ esercizio1.py
@@ -2,7 +2,7 @@
 SHAPEFILE CREATOR - automating geographic analysis stuff
 
 This script allows the user to create ESRI Shapefiles (point vector) from DEM raster and csv files:
-    1) DEM raster reprojection (from Geographic rs to Projected rs)
+    1) DEM raster re-projection (from Geographic rs to Projected rs)
     2) Sample DEM to get height information pixel-by-pixel, adding it to csv files as a field
     3) Create ESRI shapefiles from csv files
 
@@ -15,7 +15,7 @@ This file can also be imported as a module and contains the following
 functions:
 
     * sample - the sample function
-    * main - the main function of the script
+    * main -> re-projection of dem + sample to all csv files in the wd
 """
 
 import os
@@ -62,7 +62,13 @@ def sample(input_csv, input_raster):
     layer.CreateField(ogr.FieldDefn('ycoord', ogr.OFTReal))
     layer.CreateField(ogr.FieldDefn('height', ogr.OFTReal))
 
-    # CYCLE THROUGH CSV ROWS IN ORDER TO ASSIGN VALUES TO SHAPEFILE FIELDS
+    # APPENDING ALL THE ROWS INSIDE A LIST L FOR THE CREATION OF NEW CSV FILE WITH HEIGHT FIELD
+    L = []
+
+    # header of new csv with added height field
+    header = ['COD_REG', 'COD_CM', 'COD_PRO', 'PRO_COM', 'COMUNE', 'NOME_TED', 'FLAG_CM', 'SHAPE_Leng', 'SHAPE_Area', 'xcoord', 'ycoord', 'height']
+
+    # CYCLE THROUGH INPUT CSV ROWS IN ORDER TO ASSIGN VALUES TO SHAPEFILE FIELDS
     for row in reader:
         list_row = []                               # New list to contain the row fields
         utm_x = float(row['xcoord'])                # get values of utm coordinates
@@ -72,10 +78,26 @@ def sample(input_csv, input_raster):
         height = band.ReadAsArray(px, py, 1, 1)     # get height from pixel coordinates on the raster values matrix
         h = float(height)
 
+        # adding the values of the corresponding fields (as keys in dictionary) of csv in the list
+        list_row.append(row['COD_REG'])
+        list_row.append(row['COD_CM'])
+        list_row.append(row['COD_PRO'])
+        list_row.append(row['PRO_COM'])
+        list_row.append(row['COMUNE'])
+        list_row.append(row['NOME_TED'])
+        list_row.append(row['FLAG_CM'])
+        list_row.append(row['SHAPE_Leng'])
+        list_row.append(row['SHAPE_Area'])
+        list_row.append(row['xcoord'])
+        list_row.append(row['ycoord'])
+        list_row.append(h)              # added height value
+
+        L.append(list_row)              # added the entire row to the L list
+
         # definition of layer attribute (element of attribute table in Qgis)
         feature = ogr.Feature(layer.GetLayerDefn())
         # setting feature fields from csv values, reading them from keys
-        feature.SetField('name', row['name'])
+        feature.SetField('name', row['COMUNE'])
         feature.SetField('xcoord', row['xcoord'])
         feature.SetField('ycoord', row['ycoord'])
         feature.SetField('height', h)                   # height field <- height read from raster sampling
@@ -94,27 +116,22 @@ def sample(input_csv, input_raster):
         # CLOSE FEATURE
         feature = None
 
-        #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!SCRIVERE LA PARTE DEI CSV DI USCITA CON LA QUOTA AGGIUNTA
-
         # end of cycle
+
+    output_csv_name = input_csv.split('.')[0]+'_QUOTA.csv'      # set new name for output_csv with height values
+    csv_output = open(output_csv_name, 'w')                     # new output csv file
+
+    # ADD HEADER AND ALL THE VALUES TO OUTPUT CSV
+    writer = csv.writer(csv_output, delimiter=',')
+    writer.writerow(header)
+    writer.writerows(L)
+
+    # CLOSE FILES
+    my_csv.close()
+    csv_output.close()
 
     # CLOSE DATA SOURCE
     data_source = None
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 def main():
@@ -123,14 +140,11 @@ def main():
     2) sample: to sample the reprojected raster
     """
     #   gdal.Warp("dem_lombardia_100m_WGS32N.tif", "dem_lombardia_100m_ED32N.tif", dstSRS='EPSG:32632')
-    sample("/csv/comuni_lomb_bergamo.csv", 'dem_lombardia_100m_ED32N.tif')
+    for csv_file in glob.glob('csv/*.csv'):
+        #sample(csv_file, 'dem_lombardia_100m_WGS32N.tif')
+        print("Done!")
+
 
 
 main()
-print("Done!")
-
-
-
-
-
 
